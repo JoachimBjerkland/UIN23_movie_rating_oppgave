04964af430222fd1a9cb0acc04f46efb9b822ba0
Joachim Bjerkland <Bjerkland2011@hotmail.no>
2023-03-28 14:44:01 +0200

    movie_search

--- movie_rating/src/App.js
+++ movie_rating/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-<Routes>
     <h1>Handleliste</h1>
-</Routes>
   );
 }
 
--- movie_rating/src/index.js
+++ movie_rating/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Routes>
+  <React.StrictMode>
     <App />
-  </Routes>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
